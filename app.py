14260e2bf800ce765db960115ad79a1002ec74bc
AravInd PB <43701328+thedemonKingx1337@users.noreply.github.com>
2024-04-22 13:15:09 +0530

    update 8

--- app.py
+++ app.py
@@ -1,42 +1,62 @@
+import warnings
 from flask import Flask, request, render_template
 import pickle
 import numpy as np
-import warnings
-from sklearn.exceptions import InconsistentVersionWarning
 
 app = Flask(__name__)
 
 # Suppress warning about inconsistent scikit-learn versions
-warnings.filterwarnings("ignore", category=InconsistentVersionWarning)
+warnings.filterwarnings("ignore", category=UserWarning)
 
 # Load the trained model from file
-with open('model.pkl', 'rb') as model_file:
-    model = pickle.load(model_file)
+
+
+def load_model():
+    with open('model.pkl', 'rb') as model_file:
+        model = pickle.load(model_file)
+    return model
+
+
+try:
+    model = load_model()
+except ValueError as e:
+    print("Error loading the model:", e)
+
+# Function to preprocess input data
+
+
+def preprocess_input(gender, age, estimated_salary):
+    Gender = 1 if gender == "Male" else 0
+    age = int(age)
+    estimated_salary = int(estimated_salary)
+    return np.array([[Gender, age, estimated_salary]])
 
 
 @app.route('/')
 def home():
-    return render_template('index.html', pred_res=None)
+    return render_template('index.html')
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
     gender = request.form['Gender']
-    if gender == "Male":
-        Gender = 1
-    elif gender == 'Female':
-        Gender = 0
-    estimated_salary = int(request.form['EstimatedSalary'])
-    age = int(request.form['Age'])
+    age = request.form['Age']
+    estimated_salary = request.form['EstimatedSalary']
 
-    # Prepare feature array
-    feature = np.array([[Gender, age, estimated_salary]])
+    # Preprocess input data
+    feature = preprocess_input(gender, age, estimated_salary)
+
+    if 'model' not in globals():
+        return render_template('index.html', pred_res="Model loading failed. Please try again later.")
 
     # Make prediction
-    prediction = model.predict(feature)
-
-    # Convert prediction to human-readable format
-    pred_res = "Will Purchase" if prediction[0] == 1 else "Will Not Purchase"
+    try:
+        prediction = model.predict(feature)
+        # Convert prediction to human-readable format
+        pred_res = "Will Purchase" if prediction[0] == 1 else "Will Not Purchase"
+    except Exception as e:
+        print("Error making prediction:", e)
+        pred_res = "Prediction failed. Please try again later."
 
     return render_template('index.html', pred_res=pred_res)
 
